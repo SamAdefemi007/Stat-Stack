86ba986ff416378a7c22dbf9e85bf54a4b9c4439
SamAdefemi007 <91951022+SamAdefemi007@users.noreply.github.com>
2022-03-26 20:54:35 +0000

    added some templates

--- statlist/views.py
+++ statlist/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from .models import Player
 
 # Create your views here.
+
+
 def home(request):
     return render(request, 'statlist/home.html')
+
+
+def profile(request):
+    playerobj = Player.objects.all().order_by("-skills__rating")[:10]
+    return render(request, 'statlist/profile.html', {'players': playerobj})
