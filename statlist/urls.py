bfe33a59df1eb395e42e3c140368abdd43f307b2
SamAdefemi007 <91951022+SamAdefemi007@users.noreply.github.com>
2022-03-29 13:18:12 +0100

    final designs

--- statlist/urls.py
+++ statlist/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('/profile', views.profile, name="profile"),
-    path('/country', views.country , name = "country")
+    path('profile/<int:id>/', views.details, name="details"),
+    path('/country', views.country, name="country"),
+    path('/club', views.club, name="club")
+
 ]
--- statlist/views.py
+++ statlist/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Player, Country
+from .models import Player, Country, Club
 from django.core.paginator import Paginator
 
 # Create your views here.
@@ -21,8 +21,35 @@ def country(request):
     countryObj = Country.objects.all().values_list(
         "countryName", flat=True).distinct()
     playerobj = {}
+    countryname = None
+    countryflag = None
     if request.method == "POST":
         countryname = request.POST["countrySet"]
+        countryflag = Country.objects.all().filter(
+            countryName=countryname).first().countryFlag
         playerobj = Player.objects.all().filter(
             playerCountry__countryName=countryname).order_by("-skills__rating")
-    return render(request, 'statlist/country.html', {'countries': countryObj, 'players': playerobj})
+    return render(request, 'statlist/country.html', {'countries': countryObj, 'players': playerobj, 'countryname': countryname, 'countryflag': countryflag})
+
+
+def club(request):
+    clubObj = Club.objects.all().values_list(
+        "clubName", flat=True).distinct()
+
+    playerobj = {}
+    clubname = None
+    clublogo = None
+    if request.method == "POST":
+        clubname = request.POST.get("clubSet")
+        clublogo = Club.objects.all().filter(
+            clubName=clubname).first().clubLogo
+        playerobj = Player.objects.all().filter(
+            playerClub__clubName=clubname).order_by("-skills__rating")
+
+    return render(request, 'statlist/club.html', {'clubs': clubObj, 'players': playerobj, 'clubname': clubname, 'clublogo': clublogo})
+
+
+def details(request, id):
+    playerobj = Player.objects.all().filter(
+        playerID=id).order_by("-skills__rating").first()
+    return render(request, 'statlist/playerdetail.html', {'player': playerobj})
